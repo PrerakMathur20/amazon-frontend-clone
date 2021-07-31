2f0b3a3b46c3cb67f2fd40657e9a1d8ad2cf77af
PrerakMathur20 <mathur.prerak@gmail.com>
2021-07-31 06:02:55 +0530

    Updates

---
+++ precache-manifest.7761cfb069d75feea8d70230c69262dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54d18299ee482cf43261eefd8b88604",
+    "url": "/amazon-frontend-clone/index.html"
+  },
+  {
+    "revision": "069c90ea79c4b6d64636",
+    "url": "/amazon-frontend-clone/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "858762d38bf087df8d3b",
+    "url": "/amazon-frontend-clone/static/css/main.4912b8a7.chunk.css"
+  },
+  {
+    "revision": "069c90ea79c4b6d64636",
+    "url": "/amazon-frontend-clone/static/js/2.860fa1c3.chunk.js"
+  },
+  {
+    "revision": "3ceb8c17f560e6682fd3523301e674b3",
+    "url": "/amazon-frontend-clone/static/js/2.860fa1c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858762d38bf087df8d3b",
+    "url": "/amazon-frontend-clone/static/js/main.b2626243.chunk.js"
+  },
+  {
+    "revision": "6e3ebe9b8a970fd95d57",
+    "url": "/amazon-frontend-clone/static/js/runtime-main.17bd677d.js"
+  }
+]);
